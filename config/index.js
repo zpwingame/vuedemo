6fbbc7ec5cecfae5a6409335257bd83ca3fcad0b
zhaopeng <1175420363@qq.com>
2017-09-24 14:20:46 +0800

    koa

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../koa/dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../koa/dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 80,
+    port: 3000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
